55fb89003bc287b6b92be2abf3cac2dfc43e7c43
Naym Hossen <naym100m@gmail.com>
2026-08-18 15:57:27 +0600

    day 01 start

---
+++ 01-arrays-hashing/valid-anagram.ts
@@ -0,0 +1,8 @@
+function isAnagram(s: string, t: string): boolean {
+    // এখানে তোমার কোড লিখবে
+    
+}
+
+// Test cases
+console.log(isAnagram("anagram", "nagaram")); // true
+console.log(isAnagram("rat", "car"));         // falsebun run valid-anagram.ts
